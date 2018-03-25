aeefe4c2c176e8580e7d2ac93316a82f7f1963f7
Jared Russell <jared@jaredrussell.me.uk>
2018-03-25 17:28:00 +0100

    Add initial page and add typescript support

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withTypescript = require('@zeit/next-typescript');
+
+module.exports = withTypescript({
+  webpack(config) {
+    return config
+  },
+  typescriptLoaderOptions: {
+    transpileOnly: false
+  }
+})
